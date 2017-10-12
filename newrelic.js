0791d6eeed86942114e38aaf4ba4349f211db2a1
Generosa Litton <generosa.litton@yahoo.com>
2017-10-12 15:47:49 -0700

    added newrelic apm

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['NYTimes Search'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'eda863d4fadab68350c831cf8506ece3865286f0',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
